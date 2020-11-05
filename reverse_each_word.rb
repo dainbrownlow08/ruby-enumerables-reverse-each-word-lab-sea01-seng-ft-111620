eaea365542808d58bfd148e8a84d66354426045e
Dain Brownlow <dain.brownlow@outlook.com>
2020-11-05 22:14:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,8 +3,10 @@ def reverse_each_word(sentence)
   
   sentence_array = sentence.split(" ")
   sentence_array.each do |word|
-    reverse_array.push(word.reverse)
+    reverse_array.push(word.reverse + " ")
   end
+  
+  
   
   reverse_array
 end
