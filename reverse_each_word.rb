a52380519229c8d4775158e9283a871fad10e185
Dain Brownlow <dain.brownlow@outlook.com>
2020-11-05 21:58:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,11 +3,18 @@ def reverse_each_word(sentence)
   reversed_string = ""
   
   sentence_array = sentence.split(" ")
-  sentence_array.each { |word| word.reverse }
+  sentence_array.each {|word| word.reverse}
+  
+  for i in sentence_array do
+    if i != sentence_array.length
+      reverse_array.push(i + " ")
+    else
+      reverse_array.push(i)
+    end
   end
   
-  while 
+  reverse_array.each {|word| reversed_string = reversed_string + word} 
     
-  whi
+  
   reversed_string
 end
